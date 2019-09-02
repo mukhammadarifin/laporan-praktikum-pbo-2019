589d25c3733f9cc9c387283aafbfc8727262ef22
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-02 23:17:45 +0700

    Add files via upload

---
+++ src/1_Pengantar_Konsep_PBO/Pertanyaan/Sepeda1841720128Arifin.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet1;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Sepeda1841720128Arifin {
+    private String merek;
+    private int kecepatan;
+    private int gear;
+    private String warna;
+    
+    public void setMerekArifin(String newValue) {
+        merek = newValue;
+    }
+    
+    public void gantiGearArifin(int newValue) {
+        gear = newValue;
+    }
+    
+    public void tambahKecepatanArifin(int increment) {
+        kecepatan = kecepatan + increment;
+    }
+    
+    public void tambahWarnaArifin(String newValue){
+        warna = newValue;
+    }    
+    
+    public void remArifin(int decrement) {
+        kecepatan = kecepatan - decrement;
+    }
+    
+    public void cetakSatusArifin() {
+        System.out.println("Merek: "+merek);
+        System.out.println("Kecepatan: "+kecepatan);
+        System.out.println("Gear: "+gear); 
+        System.out.println("warna: "+warna);
+    }
+}
