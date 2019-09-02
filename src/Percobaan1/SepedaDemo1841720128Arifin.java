38aeef74e3f1a7ae9025948563d6fa270a61d273
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-02 22:31:48 +0700

    Add files via upload

---
+++ src/Percobaan1/SepedaDemo1841720128Arifin.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet1;
+
+/**
+ *
+ * @author MuFin
+ */
+public class SepedaDemo1841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Pembuatan dua buah Object sepeda
+        Sepeda1841720128Arifin spd1 = new Sepeda1841720128Arifin();
+        Sepeda1841720128Arifin spd2 = new Sepeda1841720128Arifin();
+        
+        //Pemanggilan method didalam Object sepeda
+        spd1.setMerekArifin("Polygon");
+        spd1.tambahKecepatanArifin(10);
+        spd1.gantiGearArifin(2);
+        spd1.cetakSatusArifin();
+        
+        spd2.setMerekArifin("Wiim Cycle");
+        spd2.tambahKecepatanArifin(10);
+        spd2.gantiGearArifin(2);
+        spd2.tambahKecepatanArifin(10);
+        spd2.gantiGearArifin(3);
+        spd2.cetakSatusArifin();
+    }
+    
+}
