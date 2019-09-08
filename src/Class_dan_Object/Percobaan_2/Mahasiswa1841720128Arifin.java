cd8c9ff1431d8b79d1a2460706c454761517f859
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-08 08:13:57 +0700

    Add files via upload

---
+++ src/Class_dan_Object/Percobaan_2/Mahasiswa1841720128Arifin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet2;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Mahasiswa1841720128Arifin {
+    public int nim;
+    public String nama;
+    public String alamat;
+    public String kelas;
+    
+    public void tampilBiodataArifin(){
+        System.out.println("Nim : "+nim);
+        System.out.println("Nama : "+nama);
+        System.out.println("Alamat : "+alamat);
+        System.out.println("Kelas : "+kelas);
+    }
+}
---
+++ src/Class_dan_Object/Percobaan_2/TestMahasiswa1841720128Arifin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet2;
+
+/**
+ *
+ * @author MuFin
+ */
+public class TestMahasiswa1841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mahasiswa1841720128Arifin mhs1 = new Mahasiswa1841720128Arifin();
+        mhs1.nim = 101;
+        mhs1.nama = "Lestari";
+        mhs1.alamat = "Jl. Vinolia No. 1A";
+        mhs1.kelas = "1A";
+        mhs1.tampilBiodataArifin();
+    }
+    
+}
