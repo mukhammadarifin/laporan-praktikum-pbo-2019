dc4213fd698013bf5a4b04a64cd2655f1dd85714
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-16 17:38:05 +0700

    Add files via upload

---
+++ src/3_Enkapsulasi/Tugas_3/EncapDemo1841720128Arifin.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulation;
+
+/**
+ *
+ * @author MuFin
+ */
+public class EncapDemo1841720128Arifin {
+    private String name;
+    private int age;
+    
+    public String getNameArifin(){
+        return name;
+    }
+    
+    public void setNameArifin(String newName){
+        name = newName;
+    }
+    
+    public int getAgeArifin(){
+        return age;
+    }
+    
+    public void setAge(int newAge){
+        if(newAge > 30 || newAge >= 18){
+                age = 30;
+        }
+        else{
+            age = newAge;
+        }
+    }
+}
---
+++ src/3_Enkapsulasi/Tugas_3/EncapTest1841720128Arifin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulation;
+
+/**
+ *
+ * @author MuFin
+ */
+public class EncapTest1841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EncapDemo1841720128Arifin encap = new EncapDemo1841720128Arifin();
+        encap.setNameArifin("James");
+        encap.setAge(20);
+        
+        System.out.println("Name : " + encap.getNameArifin());
+        System.out.println("Age : " + encap.getAgeArifin());
+    }
+    
+}
