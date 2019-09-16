7530d26a7692fe6be782fb21bcf71ecf11f1ba7e
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-16 17:57:58 +0700

    Add files via upload

---
+++ src/3_Enkapsulasi/Pertanyaan_1_3/MotorDemo1841720128Arifin.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package motorEncapsulation;
+
+/**
+ *
+ * @author MuFin
+ */
+public class MotorDemo1841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Motor1841720128Arifin motor = new Motor1841720128Arifin();
+        motor.printStatusArifin();
+        motor.tambahKecepatanArifin();
+        
+        motor.nyalakanMesinArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.tambahKecepatanArifin();
+        motor.printStatusArifin();
+        
+        motor.matikanMesinArifin();
+        motor.printStatusArifin();
+        
+        
+        
+    }
+    
+}
