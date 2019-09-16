13665fa244b434c62279631e1e095ba692a73fef
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-09-16 17:34:34 +0700

    Add files via upload

---
+++ src/3_Enkapsulasi/Percobaan_3/Anggota1841720128Arifin.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koperasigettersetter;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Anggota1841720128Arifin {
+
+    private String nama;
+    private String alamat;
+    private float simpanan;
+
+    public void setNamaArifin(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamatArifin(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNamaArifin() {
+        return nama;
+    }
+
+    public String getAlamatArifin() {
+        return alamat;
+    }
+
+    public float getSimpananArifin() {
+        return simpanan;
+    }
+
+    public void setorArifin(float uang) {
+        simpanan += uang;
+    }
+
+    public void pinjamArifin(float uang) {
+        simpanan -= uang;
+    }
+}
