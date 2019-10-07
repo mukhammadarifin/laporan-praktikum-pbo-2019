3d03381d581e2be2b9432b6c79d896c0f685ee69
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-10-07 17:49:50 +0700

    Add files via upload

---
+++ src/6_Inheritance/Percobaan_2/ClassA1841720128Arifin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifin.inheritance.percobaan2;
+
+/**
+ *
+ * @author MuFin
+ */
+public class ClassA1841720128Arifin {
+    private int x;
+    private int y;
+    
+    public void setXArifin(int x) {
+        this.x = x;
+    }
+
+    public void setYArifin(int y) {
+        this.y = y;
+    }
+    
+    public void getNilaiArifin(){
+        System.out.println("nilai x: "+x);
+        System.out.println("nilai y: "+y);
+    }
+}
---
+++ src/6_Inheritance/Percobaan_2/Percobaan21841720128Arifin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifin.inheritance.percobaan2;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Percobaan21841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ClassB1841720128Arifin hitung = new ClassB1841720128Arifin();
+        hitung.x=20;
+        hitung.y=30;
+        hitung.z=5;
+        hitung.getNilaiArifin();
+        hitung.getNilaiZArifin();
+        hitung.getJumlahArifin();
+    } 
+}
