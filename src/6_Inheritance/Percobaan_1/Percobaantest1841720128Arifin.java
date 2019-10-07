5bd4756ef959fa990ab4ea1f1cdc03633d58259e
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-10-07 17:48:08 +0700

    Add files via upload

---
+++ src/6_Inheritance/Percobaan_1/Percobaantest1841720128Arifin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifin.inheritance.percobaan1;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Percobaantest1841720128Arifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ClassB1841720128Arifin hitung = new ClassB1841720128Arifin();
+        hitung.x=20;
+        hitung.y=30;
+        hitung.z=5;
+        hitung.getNilaiArifin();
+        hitung.getNilaiZArifin();
+        hitung.getJumlahArifin();
+    }
+    
+}
