7bf26f7e21bb3c58c7ab01ea2b7bf28834afbe30
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2019-10-28 20:17:30 +0700

    Add files via upload

---
+++ src/9_Abstract_Class_dan_Interface/Percobaan_3/Icumlaude1841720128Arifin.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifin.AbstractClass_dan_Interface.Percobaan2;
+
+/**
+ *
+ * @author MuFin
+ */
+public interface Icumlaude1841720128Arifin {
+    public abstract void lulusArifin();
+    public abstract void meraihIPKTinggiArifin();
+}
---
+++ src/9_Abstract_Class_dan_Interface/Percobaan_3/Mahasiswa1841720128Arifin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arifin.AbstractClass_dan_Interface.Percobaan2;
+
+/**
+ *
+ * @author MuFin
+ */
+public class Mahasiswa1841720128Arifin {
+    
+    protected String nama;
+
+    public Mahasiswa1841720128Arifin(String nama) {
+        this.nama = nama;
+    }
+    
+    public void kuliahDiKampusArifin(){
+        System.out.println("Aku mahasiswa, namaku "+this.nama);
+        System.out.println("Aku berkuliah di kampus");
+    }
+}
